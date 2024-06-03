8a30c8c1fd37b7a65773b262d303d6d4ddf9faf5
xrt <xrt09280125@163.com>
2024-06-03 15:29:22 +0800

    增加报价打点日志

--- core/src/main/java/com/bingo/hotel/spa/intl/core/api/didatravel/access/DidaTravelAccess.java
+++ core/src/main/java/com/bingo/hotel/spa/intl/core/api/didatravel/access/DidaTravelAccess.java
@@ -11,7 +11,9 @@ import com.bingo.hotel.spa.intl.core.api.common.enums.SupplierSourceEnum;
 import com.bingo.hotel.spa.intl.core.api.didatravel.bean.price.DidaTravelRequest;
 import com.bingo.hotel.spa.intl.core.api.didatravel.bean.price.DidaTravelResponse;
 import com.bingo.hotel.spa.intl.core.util.HttpUtils;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DidaTravelAccess extends BaseHttpAccess<DidaTravelRequest, DidaTravelResponse> {
 
     private String host;
@@ -24,7 +26,19 @@ public class DidaTravelAccess extends BaseHttpAccess<DidaTravelRequest, DidaTrav
 
     @Override
     protected ResponseResult<DidaTravelResponse> request(String url, DidaTravelRequest request, IParser<DidaTravelResponse> parser) throws Exception {
-        ResponseResult<DidaTravelResponse> result = HttpUtils.access(url, null, JSON.toJSONString(request), parser);
+        long start = System.currentTimeMillis();
+        ResponseResult<DidaTravelResponse> result = null;
+        try {
+            result = HttpUtils.access(url, null, JSON.toJSONString(request), parser);
+            if(result == null || result.getData() == null || result.getData().getSuccess()==null || !result.isSucc()){
+                log.error("道旅报价接口异常，用时：{}，请求参数：{}", System.currentTimeMillis() - start, JSON.toJSONString(request));
+                return result;
+            }
+        } catch (Exception e){
+            log.error("道旅报价接口异常，用时：{}，请求参数：{}", System.currentTimeMillis() - start, JSON.toJSONString(request),e);
+            return result;
+        }
+        log.info("DidaTravel接口耗时：{}", System.currentTimeMillis() - start);
         return result;
     }
 
