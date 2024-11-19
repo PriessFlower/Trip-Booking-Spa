ae120afd3c155f7521c0d5c918a2c57be5ca6100
baoys01 <76671011+baoys01@users.noreply.github.com>
2024-11-19 18:02:48 +0800

    +道旅取消暂时不用

--- core/src/main/java/com/bingo/hotel/spa/intl/core/api/didatravel/utils/DidaTravelProductConvertUtil.java
+++ core/src/main/java/com/bingo/hotel/spa/intl/core/api/didatravel/utils/DidaTravelProductConvertUtil.java
@@ -63,7 +63,7 @@ public class DidaTravelProductConvertUtil {
         List<ProductRespDTO> respDTOList = Lists.newArrayList();
         for (DidaTravelResponse.HotelType hotelType : didaTravelResponse.getSuccess().getPriceDetails().getHotelList()) {
             SupplierHotelInfoRequest supplierHotelRequest = new SupplierHotelInfoRequest(hotelType.getHotelID().toString(), SupplierSourceEnum.DIDATRAVEL.getCode());
-            BaseResult<GetCityInfoBySupplierHotelIdResponse> result = hotelBaseIntlClient.getCityInfoBySupplierHotelId(supplierHotelRequest);
+//            BaseResult<GetCityInfoBySupplierHotelIdResponse> result = hotelBaseIntlClient.getCityInfoBySupplierHotelId(supplierHotelRequest);
 //            String timeZone = getTimeZone(result.getData().getCityName(), result.getData().getCountryName());
             for (DidaTravelResponse.HotelTypeRatePlan ratePlan : hotelType.getRatePlanList()) {
                 ProductInfo productInfo = ProductInfo.builder()
