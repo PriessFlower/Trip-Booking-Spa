0225f44c58eccea65defdbfe832e9a59368f30c0
baoys01 <76671011+baoys01@users.noreply.github.com>
2024-12-23 20:40:23 +0800

    +汇智取消修改

--- core/src/main/java/com/bingo/hotel/spa/intl/core/api/huitravel/utils/HuiTravelProductConvertUtil.java
+++ core/src/main/java/com/bingo/hotel/spa/intl/core/api/huitravel/utils/HuiTravelProductConvertUtil.java
@@ -62,8 +62,8 @@ public class HuiTravelProductConvertUtil {
 
     public List<ProductRespDTO> convertRatePlanVO(AvailabilityResponse availabilityResponse) {
         List<ProductRespDTO> respDTOList = Lists.newArrayList();
-        SupplierHotelInfoRequest supplierHotelRequest = new SupplierHotelInfoRequest(availabilityResponse.getResult().getPrices().get(0).getHid().toString(), SupplierSourceEnum.HUITRAVEL.getCode());
-        BaseResult<GetCityInfoBySupplierHotelIdResponse> result = hotelBaseIntlClient.getCityInfoBySupplierHotelId(supplierHotelRequest);
+//        SupplierHotelInfoRequest supplierHotelRequest = new SupplierHotelInfoRequest(availabilityResponse.getResult().getPrices().get(0).getHid().toString(), SupplierSourceEnum.HUITRAVEL.getCode());
+//        BaseResult<GetCityInfoBySupplierHotelIdResponse> result = hotelBaseIntlClient.getCityInfoBySupplierHotelId(supplierHotelRequest);
 //        String timeZone = getTimeZone(result.getData().getCityName(), result.getData().getCountryName());
         availabilityResponse.getResult().getPrices().forEach(productVO -> respDTOList.add(ProductRespDTO.builder()
                         .productId(productVO.getRpid() + "")
